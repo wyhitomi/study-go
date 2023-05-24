e80dd507ddb05001674dc9f282a9201845752463
Wagner Hitomi <wyhitomi@gmail.com>
2023-05-24 09:32:08 -0300

    feat(cards/deck): using deck type

--- 2-cards/4-defining-types/main.go
+++ 2-cards/4-defining-types/main.go
@@ -1,17 +1,16 @@
 package main
 
-import "fmt"
-
 // type deck []string
 
 func main() {
 	// cards := deck{"Ace of Diamonds", newCard()}
-	cards := deck
+	cards := deck{"Ace of Diamonds", "Two of Diamonds"} // using the type deck declared on deck.go
 	cards = append(cards, "Six of Spades")
 
-	for index, card := range cards {
-		fmt.Println(index, card)
-	}
+	// for index, card := range cards {
+	// 	fmt.Println(index, card)
+	// }
+	cards.print() // using the print() function declared on deck.go
 }
 
 func newCard() string {
