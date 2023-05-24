3ddb9b5037eb39156b4b713d961ca6dbd812ed75
Wagner Hitomi <wyhitomi@gmail.com>
2023-05-24 08:05:14 -0300

    feat(cards/slices): using slices and iterating over it

---
+++ 2-cards/3-slices-and-arrays/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	cards := []string{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Six of Spades")
+	// array := [2]string // arrays have fixed length, in this case you will get a 2 position array
+
+	for index, card := range cards {
+		fmt.Println(index, card)
+	}
+}
+
+func newCard() string {
+
+	return "Five of Diamonds"
+}
