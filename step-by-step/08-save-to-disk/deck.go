6acdbe5e6a825d99beab37692dd4eaddcf25c40d
Wagner Hitomi <wyhitomi@gmail.com>
2023-05-25 09:17:58 -0300

    feat(deck): save deck to disk

---
+++ step-by-step/08-save-to-disk/deck.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
+
+// Create a new type of 'deck'
+// which is a slice of strings
+type deck []string
+
+// Returns a new deck
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, suit+" of "+value)
+		}
+	}
+
+	return cards
+}
+
+// Prints the deck
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+
+	}
+}
+
+// Returns a dealed hand
+func deal(d deck, handsize int) (deck, deck) {
+	return d[:handsize], d[handsize:]
+}
+
+// converts a deck to a string
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+// save to file
+// for learning purposes we are using the ioutil package, however it was deprecated since go 1.16
+// for more information take a look at https://pkg.go.dev/io/ioutil
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
