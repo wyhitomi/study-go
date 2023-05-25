328b02b282eee3e76f757da191cb8fbc490f05b5
Wagner Hitomi <wyhitomi@gmail.com>
2023-05-25 06:19:16 -0300

    feat(main.go): clean up

--- 2-cards/5-functions-new-deck/main.go
+++ 2-cards/5-functions-new-deck/main.go
@@ -3,11 +3,7 @@ package main
 // type deck []string
 
 func main() {
-	// cards := deck{"Ace of Diamonds", newCard()}
 	cards := newDeck()
 
-	// for index, card := range cards {
-	// 	fmt.Println(index, card)
-	// }
-	cards.print() // using the print() function declared on deck.go
+	cards.print() // printing your deck
 }
